c37a9bfc081653269f49752e84da8a83c82a40f4
Jason Nucciarone <nucci.programming@gmail.com>
2021-03-07 14:16:44 -0500

    Write some place holder code in Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,8 @@
 package main;
 
 public class Main {
+    public static void main(String[] args){
+        String temp = "Place holder text for further development of this code base.";
+        System.out.println(temp);
+    }
 }
